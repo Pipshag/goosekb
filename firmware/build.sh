8f45f44c7092f5343a71ef8ea8133e6bd31c6dc6
Pipshag <carlchristian.ljunggren@gmail.com>
2024-05-23 10:04:59 +0200

    Merge
    changes

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ~/.virtualenvs/zmk-htnx/bin/activate
 (
 	rm -rf fw/
 	cd zmk/app/
@@ -9,6 +10,14 @@
 	west build -p -d ../../fw/right -b seeeduino_xiao_ble -- -DSHIELD=goose_right -DZMK_CONFIG="$(pwd)/../../../config"
 )
 (
+	cd zmk/app/
+	west build -p -d ../../fw/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="$(pwd)/../../../config"
+)
+(
 	cp -r fw/left/zephyr/zmk.uf2 fw/goose_left.uf2
 	cp -r fw/right/zephyr/zmk.uf2 fw/goose_right.uf2
+	cp -r fw/reset/zephyr/zmk.uf2 fw/reset_goose.uf2
+	rm -rf fw/left
+	rm -rf fw/right
+	rm -rf fw/reset
 )
