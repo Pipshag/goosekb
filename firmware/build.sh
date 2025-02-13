0b1583eb0c450439e852fea135f72880d4a16873
Pipshag <carlchristian.ljunggren@gmail.com>
2025-02-13 23:32:31 +0100

    Update

--- firmware/build.sh
+++ firmware/build.sh
@@ -3,18 +3,17 @@
 (
   rm -rf fw/*.uf2
   cd zmk/app/
-  west build -p -d ../../fw/left -b seeeduino_xiao_ble -- -DSHIELD=goose_left -DZMK_CONFIG="$(pwd)/../../../config"
-  # west build -p -d ../../fw/left -b seeeduino_xiao_ble -DSHIELD=goose_left -DZMK_CONFIG="$(pwd)/../../../config"
+  west build -p -d ../../fw/left -b seeeduino_xiao_ble -- -DSHIELD=goose_left -DZMK_CONFIG="$(pwd)/../../../config" # west build -p -d ../../fw/left -b seeeduino_xiao_ble -DSHIELD=goose_left -DZMK_CONFIG="$(pwd)/../../../config"
 )
 (
-    cd zmk/app/
-    west build -p -d ../../fw/right -b seeeduino_xiao_ble -- -DSHIELD=goose_right -DZMK_CONFIG="$(pwd)/../../../config"
-  )
-  # (
-    # cd zmk/app/
-    # west build -p -d ../../fw/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="$(pwd)/../../../config"
-  # )
-  (
+  cd zmk/app/
+  west build -p -d ../../fw/right -b seeeduino_xiao_ble -- -DSHIELD=goose_right -DZMK_CONFIG="$(pwd)/../../../config"
+)
+# (
+# cd zmk/app/
+# west build -p -d ../../fw/reset -b seeeduino_xiao_ble -- -DSHIELD=settings_reset -DZMK_CONFIG="$(pwd)/../../../config"
+# )
+(
   cp -r fw/left/zephyr/zmk.uf2 fw/goose_left.uf2
   cp -r fw/right/zephyr/zmk.uf2 fw/goose_right.uf2
   # cp -r fw/reset/zephyr/zmk.uf2 fw/reset_goose.uf2
